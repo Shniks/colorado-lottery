4be864f0e0b44f1ae98b2b01ed413e651421775e
Nikhil Shahi <shniks@gmail.com>
2022-07-28 09:34:33 -0700

    Created a new test to add grace as a contestant.

--- spec/colorado_lottery_spec.rb
+++ spec/colorado_lottery_spec.rb
@@ -98,4 +98,30 @@ RSpec.describe ColoradoLottery do
     expect(@lottery.registered_contestants).to eq(result)
   end
 
+  it 'should be able to register grace as a contestant' do
+    @lottery.register_contestant(@alexander, @pick_4)
+    @lottery.register_contestant(@alexander, @mega_millions)
+    @lottery.register_contestant(@frederick, @mega_millions)
+    @lottery.register_contestant(@winston, @cash_5)
+    @lottery.register_contestant(@winston, @mega_millions)
+
+    grace = Contestant.new({
+                            first_name: 'Grace',
+                            last_name: 'Hopper',
+                            age: 20,
+                            state_of_residence: 'CO',
+                            spending_money: 20
+                            })
+
+    grace.add_game_interest('Mega Millions')
+    grace.add_game_interest('Cash 5')
+    grace.add_game_interest('Pick 4')
+    @lottery.register_contestant(grace, @mega_millions)
+    @lottery.register_contestant(grace, @cash_5)
+    @lottery.register_contestant(grace, @pick_4)
+    result = {"Pick 4" => [@alexander, grace], "Mega Millions" => [@alexander, @frederick, @winston, grace], "Cash 5" => [@winston, grace]}
+
+    expect(@lottery.registered_contestants).to eq(result)
+  end
+
 end
