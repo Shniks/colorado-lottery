d7359d7cf26be81272723603b0134ee898c753ca
Nikhil Shahi <shniks@gmail.com>
2022-07-28 08:08:27 -0700

    Created test to check if contestant class has attributes.

--- spec/contestant_spec.rb
+++ spec/contestant_spec.rb
@@ -4,16 +4,31 @@ require './spec/spec_helper'
 require './lib/contestant'
 
 RSpec.describe Contestant do
-  it 'should be an instance of a class' do
-    alexander = Contestant.new({first_name: 'Alexander',
+
+  before do
+    @alexander = Contestant.new({first_name: 'Alexander',
                                 last_name: 'Aigiades',
                                 age: 28,
                                 state_of_residence: 'CO',
                                 spending_money: 10
                                 })
 
-    expect(alexander).to be_a Contestant 
   end
+
+  it 'should be an instance of a class' do
+    expect(@alexander).to be_a Contestant
+  end
+
+  it 'should have attributes' do
+    expect(@alexander.full_name).to eq('Alexander Aigiades')
+    expect(@alexander.age).to eq 28
+    expect(@alexander.state_of_residence).to eq ('CO')
+    expect(@alexander.spending_money).to eq 10
+    expect(@alexander.game_interests).to eq ([])
+    expect(@alexander).out_of_state?.to eq false 
+  end
+
+
 
 
 end
