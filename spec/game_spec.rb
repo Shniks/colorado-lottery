a9416b27d19360d7407fdedccf557d206b5c9867
Nikhil Shahi <shniks@gmail.com>
2022-07-28 07:54:15 -0700

    Created test to check for attributes.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -12,4 +12,14 @@ RSpec.describe Game do
     expect(mega_millions).to be_a Game
   end
 
+  it 'should have attributes' do
+    pick_4 = Game.new('Pick 4', 2)
+    mega_millions = Game.new('Mega Millions', 5, true)
+
+    expect(mega_millions.name).to eq('Mega Millions')
+    expect(mega_millions.cost).to eq 5
+    expect(mega_millions.national_drawing?).to eq true
+    expect(pick_4.national_drawing?).to eq false
+  end 
+
 end
